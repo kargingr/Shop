46cddb927c294868f12cbe5cd77c97a2cad59a83
Andrey Yun <andrey_yun@epam.com>
2014-03-02 22:18:45 +0600

    Добавил модуль security (ejb для регистрации польз-лей, проверки логина-пароля, восстановления пароля и др.)

---
+++ WebShop/security/src/main/java/com/epam/webshop/exception/NoLoginEmailCorrespondenceException.java
@@ -0,0 +1,11 @@
+package com.epam.webshop.exception;
+
+/**
+ * Created by Andrey Yun on 22.02.14.
+ */
+public class NoLoginEmailCorrespondenceException extends Exception {
+
+    public NoLoginEmailCorrespondenceException(String msg) {
+        super(msg);
+    }
+}
